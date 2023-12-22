7030336cf2c555082a6bff24b5986ed77280113c
adityarizki17 <adityaahmadrizki@gmail.com>
2023-12-22 16:59:04 +0700

    meet 9

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,3 +1,3 @@
 class GlobalVariables {
-  static const baseUrl = 'https://63d0-2404-8000-1028-10fb-c0d4-de13-83f7-f8b5.ngrok-free.app';
+  static const baseUrl = 'https://179d-2404-8000-1028-10fb-c0d4-de13-83f7-f8b5.ngrok-free.app';
 }
